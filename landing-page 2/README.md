5c0618ecce3d1cbb778043b168484b68672e86d0
Bertrand TUYISENGE <65284367+Bertrand-code@users.noreply.github.com>
2022-02-03 10:02:26 -0500

    Add files via upload

--- landing-page 2/README.md
+++ landing-page 2/README.md
@@ -1,3 +1,6 @@
+# Author
+
+Bertrand TUYISENGE, Full Stack Developper Student at Udacity through Masterschool
 # General Information
 This project is about building a landing page
 
@@ -10,11 +13,16 @@ it is done by using different languages: HTML, CSS, and JavaScript
 In a simple and better way, we use Java script to transform the static codes into dynamic
 for an easy and effective maintenance.
 
-## How to read go through my work?
+### How to read go through my work?
 
 Open your browser
 Go to index.html to see the content displayed ( You can click on different sections to see more information)
 Open app.js to see the codes in my java script 
 Last but surely not least open my style.css file to see the painting I used for my webpage. 
+
+### credits
+
+I thank the instructors at Udacity and the reviewers who put much effort to help us improve our skills everyday.
+They are our assets to achieve more in tech.
 
 ## Thanks.
--- landing-page 2/js/app.js
+++ landing-page 2/js/app.js
@@ -19,101 +19,87 @@
 */
 
 
- // Define Global Variables
- const navigation=document.querySelector('#navbar__list');
+// Define Global Variables
+const navigation = document.querySelector('#navbar__list');
 
 
 // Adding a global variable for all sections
-const sections=document.querySelectorAll("section");
+const sections = document.querySelectorAll("section");
 
-const menuLinks=document.querySelectorAll('.navbar__menu ul');
 
 //Formatting the landing page header
-const headingOne=document.getElementById('header');
-headingOne.style.backgroundColor="rgb(50,50,80)";
+const headingOne = document.getElementById('header');
+headingOne.style.backgroundColor = "rgb(50,50,80)";
 // build the nav
-    //Defining the empty variable to receive our looped information
-    let navBuilt='';
+//Defining the empty variable to receive our looped information
+let navBuilt = '';
 
-    // use of forEach function so that our forEach loop can apply to all of the sections in index.html
- function buildNav(){
-    sections.forEach(section =>{
-    //Making sure to tell how the loop function will be executed and also adding our sections hyperlinked to the page
-        
-    navBuilt +=`<li class="${section.id}"><a id="active" href="#${section.id}"> ${section.dataset.nav} </a></li>`;
+// use of forEach function so that our forEach loop can apply to all of the sections in index.html
+function buildNav() {
+    sections.forEach(section => {
+        //Making sure to tell how the loop function will be executed and also adding our sections hyperlinked to the page
 
-});
-// Let's append all we defined in the inner html
+        navBuilt += `<li class="${section.id}"><a id="active" href="#${section.id}"> ${section.dataset.nav} </a></li>`;
 
-navigation.innerHTML=navBuilt;
+    });
+    // Let's append all we defined in the inner html
+
+    navigation.innerHTML = navBuilt;
 
 
- }
+}
 // Call the function
 buildNav();
 // Adding 'Your active Class' to all the sections viewed while scrolling on the page;
-           
-const viewPort=(section)=>{
-    const activeClass=section.getBoundingClientRect().top<150 && section.getBoundingClientRect().bottom>=150;
+
+const viewPort = (section) => {
+    const activeClass = section.getBoundingClientRect().top < 150 && section.getBoundingClientRect().bottom >= 150;
     return activeClass;
 
 }
 
 //Implementation of the actual function
-const scrollFunction=(sec)=>{
-   
-    sections.forEach(section=>{
-    ////Add class 'active' to section when near top of viewport
-    if(viewPort(section)){
+const scrollFunction = (sec) => {
+
+    sections.forEach(section => {
+        ////Add class 'active' to section when near top of viewport
+
+        const navlink = document.querySelector(`a[href="#${section.getAttribute("id")}"]`);
+        const sectionTop = section.offsetTop;
+        console.log(sectionTop)
+         const sectionHeight = section.clientHeight;
+
+        if (viewPort(section) &&(pageYOffset >= (sectionTop - sectionHeight / 4))) {
             section.classList.add('your-active-class');
-            section.style.cssText='background-color:crimson';
+            section.style.cssText = 'background-color:crimson';
             section.preventDefault;
-            
-        } 
+
+            navlink.style.backgroundColor = "crimson";
+
+        }
         //Remove the 'active' to section
-        else{
-        section.classList.remove('your-active-class');
-        section.style.cssText="background-color:black";
+        else {
+            section.classList.remove('your-active-class');
+            section.style.cssText = "background-color:black";
+
+            navlink.style.backgroundColor = "";
 
         }
     })
 }
 document.addEventListener('scroll', scrollFunction);
 
- 
-// Scroll to section on link click
 
-  const eventWhenScrollingDown=()=>{
+// Adding an Event Listener to the sections while 
+
+const eventWhenScrollingDown = () => {
     sections.forEach(section => {
-        section.addEventListener("click", function(e){
+        section.addEventListener("click", function (e) {
             e.preventDefault();
-                section.scrollIntoView({behavior: "smooth"})
+            section.scrollIntoView({ behavior: "smooth" })
         });
     });
-}; 
+};
 
 // Calling the function so that when the user scrolls down, the event listener can be added to the sections
 eventWhenScrollingDown()
-
-//Highlighting the navItems while Scrolling down;
-window.addEventListener('scroll', ()=>{
-
-    let current='';
-    sections.forEach(section=>{
-       const sectionTop=section.offsetTop;
-       const sectionHeight=section.clientHeight;
-       if(pageYOffset>=sectionTop){
-           current=section.getAttribute('id');
-       }
-
-    })
-    menuLinks.forEach(menu=>{
-
-        menu.classList.remove('menu__link');
-        if(menu.classList.contains(current)){
-            console.log(menu);
-            menu.classList.add('menu__link')
-            
-        }
-    })
-})
